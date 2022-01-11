f760ef30dc096495b62d71bf4ca902d0bb1b05e3
Will Huang <whuang26@gmail.com>
2022-01-11 17:16:01 -0500

    create a second chart, change the color of the line in second chart to green

--- scripts/chart.js
+++ scripts/chart.js
@@ -28,7 +28,7 @@ function drawLineChart(idAttr){
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.close); });
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("#apple-stock-chart-container").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .attr("class", "chart")
